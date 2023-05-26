579728a29d9df70eceac391b174cefbe8eade1e0
Marcus Pabst <marcus.pabst@cosmoconsult.com>
2023-05-26 14:57:07 +0200

    Fixed package references

--- EAI.AzureStorage/StorageQueue.cs
+++ EAI.AzureStorage/StorageQueue.cs
@@ -4,7 +4,6 @@ using EAI.General.Storage;
 using EAI.General.Cache;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
