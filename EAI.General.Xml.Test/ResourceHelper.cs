1e32164e30bbb337b2a702b098e026e2f9996433
Mark Zöltsch <mzoeltsch10299@cosmoconsult.com>
2023-06-05 19:11:40 +0200

    added base test

---
+++ EAI.General.Xml.Test/ResourceHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace EAI.General.Xml.Test
+{
+    internal static class ResourceHelper
+    {
+        private static readonly string XmlFolder = "XmlTestDocs";
+
+        private static readonly string Xml_ChemicalTest1 = "ZDebmas07_SampleChemicalIndustry01.xml";
+
+        internal static XDocument? XmlChemicalTest01
+        {
+            get
+            {
+                var str = GetXml(Xml_ChemicalTest1);
+                if (string.IsNullOrWhiteSpace(str))
+                    return null;
+
+                return XDocument.Parse(str);
+            }
+        }
+
+        internal static string? GetXml(string name) => GetString($"EAI.General.Xml.Test.{XmlFolder}.{name}");
+
+        private static string? GetString(string resourceName)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            using (Stream? stream = assembly?.GetManifestResourceStream(resourceName))
+            {
+                if (stream == null)
+                    return null;
+
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    return reader.ReadToEnd();
+
+                }
+            }
+        }
+    }
+}
--- EAI.General.Xml.Test/UnitTest1.cs
+++ EAI.General.Xml.Test/UnitTest1.cs
@@ -2,10 +2,13 @@ namespace EAI.General.Xml.Test
 {
     public class UnitTest1
     {
+        
         [Fact]
-        public void Test1()
+        public void TestResources01()
         {
+            var data = ResourceHelper.XmlChemicalTest01;
 
+            Assert.NotNull(data);
         }
     }
 }
