a13d9830c0a39389a5c8552882c94e9165020019
Mark Zöltsch <mzoeltsch10299@cosmoconsult.com>
2023-06-16 17:20:21 +0200

    add sap general lib

---
+++ EAI.General.SAPECC/Extensions/StringExtension.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+
+namespace EAI.General.SAPECC.Extensions
+{
+    public static class StringExtension
+    {
+        public static string RemoveLeadingZeros(this string original)
+        {
+            if (string.IsNullOrWhiteSpace(original))
+                return original;
+
+            var trimmed = original.TrimStart(new char[] { '0' });
+
+            if (trimmed.Length == 0)
+                return "0";
+
+            return trimmed;
+        }
+
+        public static DateTime ParseSAPDate(this string sap, bool whenEmptySetNow = false)
+        {
+            if (string.IsNullOrWhiteSpace(sap) || sap == "00000000")
+                if (whenEmptySetNow)
+                    return DateTime.Now.Date;
+                else
+                    return null;
+
+            return DateTime.ParseExact(sap, "yyyyMMdd", CultureInfo.InvariantCulture, DateTimeStyles.None);
+        }
+
+        public static DateTime? ParseSAPTime(this string sap, bool whenEmptySetNow = false)
+        {
+            if (string.IsNullOrWhiteSpace(sap) || sap == "000000")
+                if (whenEmptySetNow)
+                    return DateTime.Now;
+                else
+                    return null;
+
+            return DateTime.ParseExact(sap, "HHmmss", CultureInfo.InvariantCulture, DateTimeStyles.None);
+        }
+    }
+}
