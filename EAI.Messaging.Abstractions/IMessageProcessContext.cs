881e326165bc1ae4ff3058568fb926abb200203f
Marcus Pabst <marcus.pabst@cosmoconsult.com>
2023-07-13 17:07:41 +0200

    Messaging -base messaging interfaces

---
+++ EAI.Messaging.Abstractions/IMessageProcessContext.cs
@@ -0,0 +1,12 @@
+﻿using EAI.General;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EAI.Messaging.Abstractions
+{
+    public interface IMessageProcessContext
+    {
+        ProcessContext ProcessContext { get; set; }
+    }
+}
---
+++ EAI.Messaging.Abstractions/IMessageTransactionKey.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EAI.Messaging.Abstractions
+{
+    public interface IMessageTransactionKey
+    {
+        string TransactionKey { get; }
+    }
+}
