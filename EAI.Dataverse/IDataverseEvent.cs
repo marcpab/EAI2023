cfd48c1cfe8b35aa673fbb733a2056655e6da872
Marcus Pabst <marcus.pabst@cosmoconsult.com>
2023-07-19 16:14:40 +0200

    Dataverse -Added aditional event information support

---
+++ EAI.Dataverse/IDataverseEvent.cs
@@ -0,0 +1,17 @@
+﻿using EAI.OData;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EAI.Dataverse
+{
+    public interface IDataverseEvent
+    {
+        string Action { get; set; }
+        string PrimaryEntityName { get; set; }
+
+    }
+}
