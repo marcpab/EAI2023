f63da34a65dcef34239837a8a3aa5e67385cd0ae
Mark Zöltsch <mzoeltsch10299@cosmoconsult.com>
2023-08-14 17:45:06 +0200

    bugfix tlog

--- EAI.Logging.SQL/SQLStatements/tLog.cs
+++ EAI.Logging.SQL/SQLStatements/tLog.cs
@@ -2,11 +2,21 @@
 using System.Data.SqlClient;
 using System.Data;
 using EAI.Logging.Model;
+using System.Text;
+using System.Security.Cryptography;
 
 namespace EAI.Logging.SQL
 {
     internal class tLog
     {
+        static Int32 sha256(string content)
+        {
+            var crypt = new SHA256Managed();
+            byte[] crypto = crypt.ComputeHash(Encoding.ASCII.GetBytes(content));
+            
+            return BitConverter.ToInt32(crypto, 0);
+        }
+
         public static
 #if NETSTANDARD2_1
             async Task<long> 
@@ -26,13 +36,13 @@ namespace EAI.Logging.SQL
 
                 cmd.AddParameter("@Id_Level", SqlDbType.Int, 0, (int)item.Level);
                 cmd.AddParameter("@Id_Stage", SqlDbType.Int, 0, item.StageId);
-                cmd.AddParameter("@Service", SqlDbType.NVarChar, 200, item.Service ?? string.Empty);
-                cmd.AddParameter("@ParentProcessId", SqlDbType.NVarChar, 40, item.Transaction ?? string.Empty);
-                cmd.AddParameter("@ParentProcessHash", SqlDbType.Int, 0, item.Transaction);
-                cmd.AddParameter("@ProcessId", SqlDbType.NVarChar, 40, item.ChildTransaction ?? string.Empty);
-                cmd.AddParameter("@MessageId", SqlDbType.NVarChar, 200, item.TransactionKey ?? string.Empty);
+                cmd.AddParameter("@Service", SqlDbType.NVarChar, 80, item.Service ?? string.Empty);
+                cmd.AddParameter("@ParentProcessId", SqlDbType.NVarChar, 50, item.Transaction ?? string.Empty);
+                cmd.AddParameter("@ParentProcessHash", SqlDbType.Int, 0, sha256(item.Transaction));
+                cmd.AddParameter("@ProcessId", SqlDbType.NVarChar, 50, item.ChildTransaction ?? string.Empty);
+                cmd.AddParameter("@MessageId", SqlDbType.NVarChar, 80, item.TransactionKey ?? string.Empty);
                 cmd.AddParameter("@Text", SqlDbType.NVarChar, int.MaxValue, item.Description ?? string.Empty);
-                cmd.AddParameter("@MessageName", SqlDbType.NVarChar, 200, item.LogMessage?.Operation ?? string.Empty);
+                cmd.AddParameter("@MessageName", SqlDbType.NVarChar, 80, item.LogMessage?.Operation ?? string.Empty);
                 cmd.AddParameter("@Id_Message", SqlDbType.BigInt, 0, (object)idMessage ?? DBNull.Value);
                 cmd.AddParameter("@CreatedOnUTC", SqlDbType.DateTime, 0, (object)item.CreatedOnUTC ?? DBNull.Value);
 
