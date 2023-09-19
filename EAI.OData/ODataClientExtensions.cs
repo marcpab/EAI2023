768f41a0685c48a46eab98a610efedeb14caaac5
Marcus Pabst <marcus.pabst@cosmoconsult.com>
2023-09-19 11:35:59 +0200

    OData -Added Accept und Prefer headers

--- EAI.OData/ODataClientExtensions.cs
+++ EAI.OData/ODataClientExtensions.cs
@@ -7,6 +7,17 @@ namespace EAI.OData
 {
     public static class ODataClientExtensions
     {
+        private static Dictionary<string, string> _defaultRequestHeaders = new Dictionary<string, string>
+        {
+            {"Accept", "application/json" },
+            {"Prefer", "return=representation"}
+        };
+
+        private static Dictionary<string, string> _deleteRequestHeaders = new Dictionary<string, string>
+        {
+            {"Prefer", "return=minimal"}
+        };
+
         private const string FetchMethod = "GET";
         private const string CreateMethod = "POST";
         private const string UpdateMethod = "PATCH";
@@ -14,22 +25,22 @@ namespace EAI.OData
 
         public static FluentRequest Fetch(this ODataClient client, ODataQuery query) 
         { 
-            return new FluentRequest(client, query, FetchMethod, null);
+            return new FluentRequest(client, query, FetchMethod, null, _defaultRequestHeaders);
         }
 
         public static FluentRequest Create(this ODataClient client, ODataQuery query, object content)
         {
-            return new FluentRequest(client, query, CreateMethod, content);
+            return new FluentRequest(client, query, CreateMethod, content, _defaultRequestHeaders);
         }
 
         public static FluentRequest Update(this ODataClient client, ODataQuery query, object content)
         {
-            return new FluentRequest(client, query, UpdateMethod, content);
+            return new FluentRequest(client, query, UpdateMethod, content, _defaultRequestHeaders);
         }
 
         public static FluentRequest Delete(this ODataClient client, ODataQuery query)
         {
-            return new FluentRequest(client, query, DeleteMethod, null);
+            return new FluentRequest(client, query, DeleteMethod, null, _deleteRequestHeaders);
         }
     }
 }
