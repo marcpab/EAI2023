0453fad5ed36efa31e92d93b900a6be5a9b78a74
Marcus Pabst <marcus.pabst@cosmoconsult.com>
2023-10-19 12:38:48 +0200

    AzureFunctions - Added simple trigger

---
+++ EAI.AzureFunctions/SimpleTriggerFunction.cs
@@ -0,0 +1,68 @@
+﻿using Azure.Core;
+using EAI.General;
+using EAI.LoggingV2.Levels;
+using EAI.Messaging.Abstractions;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Diagnostics.Tracing;
+using System.Threading.Tasks;
+
+namespace EAI.AzureFunctions
+{
+    public abstract class SimpleTriggerFunction<requestT> : Function
+    {
+        public async Task RunTestAsync(requestT requestMessage)
+        {
+            await InitializeTestAsync();
+
+            SetupProcessContext(null);
+
+            try
+            {
+                Log?.Start<Info>(nameof(requestMessage), requestMessage, "Start (test)");
+
+                SetMetadataAndLog(requestMessage);
+
+                await ProcessRequestAsync(requestMessage);
+
+                Log?.Success<Info>();
+            }
+            catch (Exception ex)
+            {
+                Log?.Failed<Error>(ex);
+            }
+
+            if (Log != null)
+                await Log.FlushAsync();
+        }
+
+        protected async Task RunQueueRequestAsync(FunctionContext functionContext, requestT requestMessage)
+        {
+            await InitializeAsync(functionContext);
+
+            SetupProcessContext(functionContext.InvocationId);
+
+            try
+            {
+
+                Log?.Start<Info>(nameof(requestMessage), requestMessage);
+
+                SetMetadataAndLog(requestMessage);
+
+                await ProcessRequestAsync(requestMessage);
+
+                Log?.Success<Info>();
+            }
+            catch (Exception ex)
+            {
+                Log?.Failed<Error>(ex);
+            }
+
+            if(Log != null) 
+                await Log.FlushAsync();
+        }
+
+        protected abstract Task ProcessRequestAsync(requestT requestMessage);
+    }
+}
